b28452e820be412bba835144a1a56745c610c427
superedriver <superedriver@gmail.com>
2016-08-22 12:37:39 +0300

    added validation date_finish >= date_start

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,15 @@
 class Event < ApplicationRecord
   belongs_to :user
   validates :user_id, :date_start, :date_finish, presence: true
+  validate :finish_is_greater_than_start
+
+  private
+
+  def finish_is_greater_than_start
+    errors.add(:date_finish, I18n.t("activerecord.errors.models.event.attributes.date_finish.not_greater")) if date_start_is_grate?
+  end
+
+  def  date_start_is_grate?
+    date_start.present? && date_finish.present? && date_start >= date_finish
+  end
 end
--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -3,7 +3,7 @@ FactoryGirl.define do
     name "Meeting"
     description "Meeting with Projector"
     date_start "2016-07-22 14:05:29"
-    date_finish "2016-07-22 14:05:29"
+    date_finish "2016-07-22 14:05:30"
     association :user
   end
 end
